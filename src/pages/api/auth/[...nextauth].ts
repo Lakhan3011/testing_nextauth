7c445580e79148b3cc33f870a996140832972f4d
Lakhan lal <lakhan191136@dei.ac.in>
2024-02-27 18:12:18 +0530

    Done with testing

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,71 +1,67 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import {Provider} from "next-auth/providers";
-import { ensureDbConnected } from '@/lib/dbConnect';
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { Provider } from "next-auth/providers";
+import { ensureDbConnected } from "@/lib/dbConnect";
 import { Admin } from "@/lib/db";
 
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        GoogleProvider({
-            clientId: process.env.NEXT_GOOGLE_CLIENT_ID,
-            clientSecret: process.env.NEXT_GOOGLE_CLIENT_SECRET,
-        }),
-        CredentialsProvider({
-            id: "credentials",
-            name: "Credentials",
-            type: "credentials",
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials, req) {
-                await ensureDbConnected()
-                if (!credentials) {
-                    return null;
-                }
-                const username = credentials.username;
-                const password = credentials.password;
-                // Add logic here to look up the user from the credentials supplied
-                const admin = await Admin.findOne({ username });
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      type: "credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "name" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        await ensureDbConnected();
+        if (!credentials) {
+          return null;
+        }
+        const username = credentials.username;
+        const password = credentials.password;
+        // Add logic here to look up the user from the credentials supplied
+        const admin = await Admin.findOne({ username });
 
-                if (!admin) {
-                    const obj = { username: username, password: password };
-                    const newAdmin = new Admin(obj);
-                    let adminDb = await newAdmin.save();
-                    console.log(adminDb);
-                    return {
-                        id: adminDb._id,
-                        email: adminDb.username,
-                    }
-                } else {
-                    //TODO:: Make this safer, encrypt passwords
-                    if (admin.password !== password) {
-                        return null
-                    }
-                    // User is authenticated
-                    return {
-                        id: admin._id,
-                        email: admin.username,
-                    }
-                }
-            }
-        }),
-    ] as Provider[],
-    secret: process.env.NEXTAUTH_SECRET,
-    session: {
-        strategy: "jwt",
-        maxAge: 30 * 24 * 60 * 60, // 30 days
-    },
-    jwt: {
-        encryption: true
-    },
-}
+        if (!admin) {
+          const obj = { username: username, password: password };
+          const newAdmin = new Admin(obj);
+          let adminDb = await newAdmin.save();
+          console.log(adminDb);
+          return {
+            id: adminDb._id,
+            email: adminDb.username,
+          };
+        } else {
+          //TODO:: Make this safer, encrypt passwords
+          if (admin.password !== password) {
+            return null;
+          }
+          // User is authenticated
+          return {
+            id: admin._id,
+            email: admin.username,
+          };
+        }
+      },
+    }),
+  ] as Provider[],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  jwt: {
+    encryption: true,
+  },
+};
 
-export default NextAuth(authOptions)
-
-
-
-
+export default NextAuth(authOptions);
--- src/pages/ssr.tsx
+++ src/pages/ssr.tsx
@@ -1,48 +1,59 @@
-
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 import { Button, Typography } from "@mui/material";
-import {signIn, useSession, signOut} from "next-auth/react"
-import {getServerSession} from "next-auth";
-import { authOptions } from '@/pages/api/auth/[...nextauth]'
+import { signIn, useSession, signOut } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
-export default function Ssr({session}) {
-    return (
-        <div style={{height: 60, background: "white", padding: 10}}>
-            {session.data && <div style={{display: "flex", justifyContent: "space-between"}}>
-                <Typography variant={"h4"} style={{color: "black"}}>
-                    {session.data.user?.email}
-                </Typography>
-                <div>
-                    <Button variant={"contained"} onClick={() => signOut()}>Logout</Button>
-                </div>
-            </div>}
-            {!session.data && <div style={{display: "flex", justifyContent: "space-between"}}>
-                <Typography variant={"h4"} style={{color: "black"}}>
-                    Coursera
-                </Typography>
-                <div>
-                    <Button variant={"contained"} onClick={() => signIn()}>Sign up</Button>
-                </div>
-            </div>}
+export default function Ssr({ session }) {
+  return (
+    <div style={{ height: 60, background: "white", padding: 10 }}>
+      {session.user && (
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography variant={"h4"} style={{ color: "black" }}>
+            {session.user?.email}
+          </Typography>
+          <div>
+            <Button variant={"contained"} onClick={() => signOut()}>
+              Logout
+            </Button>
+          </div>
         </div>
-    )
+      )}
+      {!session.user && (
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography variant={"h4"} style={{ color: "black" }}>
+            Coursera
+          </Typography>
+          <div>
+            <Button variant={"contained"} onClick={() => signIn()}>
+              Sign up
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
-    const session = await getServerSession(context.req, context.res, authOptions)
+  const session = await getServerSession(context.req, context.res, authOptions);
 
-    if (!session) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        }
-    }
-
+  if (!session) {
     return {
-        props: {
-            session,
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session: {
+        user: {
+          email: session.user?.email,
         },
-    }
+      },
+    },
+  };
 }
